0a0b227e6016fffa47b95fb7d2dd1c76e4efc611
Elijah Silverman <elijahmsilverman@gmail.com>
2021-10-20 14:00:40 -0400

    commit tree c0a783a28ef3804b86dbdb548ffc26c88c0861b7

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+const submitData = function(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {  
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        }),
+    })
+    .then(res => res.json())
+    .then(res => document.body.append(res.id))
+    .catch(error => {
+        document.body.append(error.message)
+    })
+}
+
